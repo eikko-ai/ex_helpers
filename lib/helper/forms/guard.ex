6b0472064cd51c33e2631bbb540a1e129beea7cb
Pedro Pereira <talkwithpedro@gmail.com>
2021-09-06 17:07:50 +0100

    Refactor Forms -> Guard

--- lib/helper/forms/guard.ex
+++ lib/helper/forms/guard.ex
@@ -1,23 +1,23 @@
 defmodule Helper.Forms.Guard do
-  def submitted_too_fast?(time_str, diff \\ 3),
-    do:
-      Time.diff(
-        time_from_string(time_str),
-        Time.utc_now(),
-        :second
-      ) < diff
+  def time(),
+    do: Time.utc_now()
 
-  defp time_from_string(time_str),
-    do:
-      time_str
+  def submitted_too_fast?(start_time, min_allowed \\ 2) do
+    time_taken =
+      start_time
       |> Time.from_iso8601()
       |> parse_time_result()
+      |> Time.diff(time(), :second)
+      |> abs()
+
+    time_taken < min_allowed
+  end
 
   defp parse_time_result({:ok, time}),
     do: time
 
   defp parse_time_result(_),
-    do: Time.utc_now()
+    do: time()
 
   def honeypot_filled?(honeypot_str),
     do: byte_size(honeypot_str) > 0
